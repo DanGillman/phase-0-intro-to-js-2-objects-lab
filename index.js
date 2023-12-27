a9d3823c877d98d62b347d33402776c02dc3c0a9
dangillman <dangillman93@gmail.com>
2023-12-27 12:35:21 -0500

    Added content to index.js, completed lab

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Write your solution in this file!
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+      ...employee,
+      [key]: value
+    };
+  }
+  
+  let originalEmployee = {
+    name: "Sam",
+    streetAddress: "11 Broadway"
+  };
+  
+  let updatedEmployee = updateEmployeeWithKeyAndValue(originalEmployee, "name", "Carl");
+  
+  console.log(updatedEmployee);
+  
+  
+  // Destructively update employee
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  let employee = {
+    name: "Sam",
+    streetAddress: "12 Broadway"
+  };
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    const clonedEmployee = { ...employee };
+    delete clonedEmployee[key];
+    return clonedEmployee;
+  }
+  
+  let clonedEmployee = {
+    name: "Sam",
+    streetAddress: "11 Broadway"
+  };
+  
+  let updatedClonedEmployee = deleteFromEmployeeByKey(clonedEmployee, "name");
+  console.log(updatedClonedEmployee);
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
